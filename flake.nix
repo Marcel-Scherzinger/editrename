a0095a40561e4c84823fddd201acddd78c6cc583
Marcel Scherzinger <>
2026-09-01 14:09:38 +0200

    add default overlay

--- flake.nix
+++ flake.nix
@@ -7,23 +7,24 @@
   };
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
-      imports = [];
+      imports = [
+        inputs.flake-parts.flakeModules.easyOverlay
+      ];
+
       systems = ["x86_64-linux" "aarch64-linux" "aarch64-darwin"];
       perSystem = {
-        # config,
+        config,
         self',
         # inputs',
         pkgs,
         # system,
         ...
       }: {
-        # Per-system attributes can be defined here. The self' and inputs'
-        # module parameters provide easy access to attributes of the same
-        # system.
-
-        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
-
         formatter = pkgs.alejandra;
+
+        overlayAttrs = {
+          inherit (config.packages) editrename;
+        };
 
         packages.editrename = pkgs.rustPlatform.buildRustPackage {
           name = "editrename";
